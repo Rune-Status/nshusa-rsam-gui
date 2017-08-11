fe7f7fffdc3aa120ed1e398bd3487b7ca46402c5
Chad <nshusa99@gmail.com>
2017-08-11 02:34:19 -0500

    Renamed package

---
+++ src/main/java/io/nshusa/AppData.java
@@ -0,0 +1,84 @@
+package io.nshusa;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.nshusa.model.ArchiveMeta;
+import javafx.scene.image.Image;
+
+public final class AppData {
+	
+	public static final Path resourcePath = Paths.get(System.getProperty("user.home") + File.separator + ".rsam");
+	
+	public static final Path storeResourcePath = resourcePath.resolve("stores.txt");
+	
+	public static final Path archiveResourcePath = resourcePath.resolve("archives.txt");
+	
+	public static final Path hashResourcePath = resourcePath.resolve("hashes.txt");
+	
+	public static final Image indexIcon = new Image(App.class.getResourceAsStream("/images/index_icon.png"));
+
+	public static final Image datIcon = new Image(App.class.getResourceAsStream("/images/dat_icon.png"));
+
+	public static final Image idxIcon = new Image(App.class.getResourceAsStream("/images/idx_icon.png"));
+
+	public static final Image textIcon = new Image(App.class.getResourceAsStream("/images/text_icon.png"));
+
+	public static final Image midiIcon = new Image(App.class.getResourceAsStream("/images/midi_icon.png"));
+
+	public static final Image pngIcon = new Image(App.class.getResourceAsStream("/images/dat_icon.png"));
+
+	public static final Image fileIcon = new Image(App.class.getResourceAsStream("/images/file_icon.png"));
+	
+	public static final Image gzipIcon = new Image(App.class.getResourceAsStream("/images/gzip_icon.png"));
+	
+	public static final Image addIcon = new Image(App.class.getResourceAsStream("/images/action_add.png"));
+	
+	public static final Image deleteIcon = new Image(App.class.getResourceAsStream("/images/action_delete.png"));
+	
+	public static final Image downloadIcon = new Image(App.class.getResourceAsStream("/images/download.png"));
+	
+	public static final Image clearIcon = new Image(App.class.getResourceAsStream("/images/clear_icon.png"));
+	
+	private AppData() {
+		
+	}
+	
+	public static final Map<Integer, String> storeNames = new HashMap<>();
+	
+	public static final Map<Integer, ArchiveMeta> archiveMetas = new HashMap<>();
+	
+	public static final Map<Integer, String> commonHashNames = new HashMap<>();
+	
+	public static Image getIcon(String extension) {
+		
+		switch (extension) {		
+		
+			case "dat":
+				return datIcon;
+			
+			case "idx":
+				return idxIcon;
+				
+			case "txt":
+				return textIcon;
+				
+			case "midi":
+				return midiIcon;
+		
+			case "png":
+				return pngIcon;
+				
+			case "gz":
+				return gzipIcon;
+
+		}
+		
+		return fileIcon;
+		
+	}	
+
+}
---
+++ src/main/java/io/nshusa/util/GZipUtils.java
@@ -0,0 +1,36 @@
+package io.nshusa.util;
+
+import java.util.zip.GZIPInputStream;
+
+/**
+ * A static-utility class that contains useful methods for GZip.
+ * 
+ * @author Freyr
+ */
+public final class GZipUtils {
+	
+	private GZipUtils() {
+		
+	}
+	
+	/**
+	 * Determines if a byte array is compressed. The java.util.zip GZip
+	 * implementaiton does not expose the GZip header so it is difficult to
+	 * determine if a string is compressed.
+	 * 
+	 * @param bytes an array of bytes
+	 * 
+	 * @return true if the array is compressed or false otherwise
+	 * 
+	 * @throws java.io.IOException if the byte array couldn't be read
+	 */
+	public static boolean isGZipped(byte[] bytes) {
+		if ((bytes == null) || (bytes.length < 2)) {
+			return false;
+		} else {
+			return ((bytes[0] == (byte) (GZIPInputStream.GZIP_MAGIC))
+					&& (bytes[1] == (byte) (GZIPInputStream.GZIP_MAGIC >> 8)));
+		}
+	}
+
+}
