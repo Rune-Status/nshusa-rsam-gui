bbd93fc314ca1334cf328c048e79482d36aab92f
Chad <freesunfury@gmail.com>
2017-09-21 04:40:40 -0500

    Fixed issue #15

--- src/io/nshusa/controller/ImageArchiveController.java
+++ src/io/nshusa/controller/ImageArchiveController.java
@@ -60,6 +60,7 @@ public class ImageArchiveController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		imageView.setPreserveRatio(true);
 		treeView.setRoot(new TreeItem<>("Root"));
 
 		treeView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
@@ -84,6 +85,32 @@ public class ImageArchiveController implements Initializable {
 				contextMenu.getItems().add(exportMI);
 
 				treeView.setContextMenu(contextMenu);
+
+				Image image = imageView.getImage();
+
+				System.out.println(image.getWidth() + " " + image.getHeight());
+
+				double width = image.getWidth();
+				double height = image.getHeight();
+
+				if (image.getWidth() > 512) {
+					width = 512;
+				}
+
+				if (image.getHeight() > 512) {
+					height = 512;
+				}
+
+				if (image.getWidth() < 16) {
+					width = 16;
+				}
+
+				if (image.getHeight() < 16) {
+					height = 16;
+				}
+
+				this.imageView.setFitWidth(width);
+				this.imageView.setFitHeight(height);
 
 				this.imageView.setImage(imageView.getImage());
 
@@ -146,7 +173,24 @@ public class ImageArchiveController implements Initializable {
 
 						Image image = SwingFXUtils.toFXImage(bImage, null);
 
-						parent.getChildren().add(new TreeItem<String>(Integer.toString(frame), new ImageView(image)));
+						ImageView imageView = new ImageView(image);
+
+						if (image.getWidth() > 128) {
+							imageView.setFitWidth(128);
+						}
+
+						if (image.getHeight() > 128) {
+							imageView.setFitHeight(128);
+						}
+
+						if (image.getWidth() < 16 || image.getHeight() < 32) {
+							imageView.setFitWidth(16);
+							imageView.setFitHeight(16);
+						}
+
+						imageView.setPreserveRatio(true);
+
+						parent.getChildren().add(new TreeItem<>(Integer.toString(frame), imageView));
 					}
 
 					double progress = ((double) (i + 2) / archive.getEntries().size()) * 100;
